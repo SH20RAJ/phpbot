defa2aa0b78d652acf76d89fdea35e84e24d527d
sh20raj <shaswatraj3@gmail.com>
2024-07-17 22:04:38 +0530

    Refactor index.php to remove unnecessary code and improve readability

--- play/index.php
+++ play/index.php
@@ -2,12 +2,9 @@
 
 include "../phpgram.php";
 
-$phpgram = new PHPGram();
-
-$phpgram->setWebhook("https://phpbot.sh20raj.com/play/");
-
 $token = '7337693933:AAGKjpcWREFw5u4U_efy0UkRbq692QxC87k';
 $bot = new PhpGram($token);
+
 
 $botInfo = $bot->getMe();
 echo 'Bot Username: ' . $botInfo['result']['username'] . PHP_EOL;

4d9efbe4756acfdbe085cac6addaf0e4521c5a0c
sh20raj <shaswatraj3@gmail.com>
2024-07-17 22:11:19 +0530

    Refactor index.php to remove unnecessary code and improve readability

--- play/index.php
+++ play/index.php
@@ -1,12 +1,5 @@
 <?php
 
-include "../phpgram.php";
 
-$token = '7337693933:AAGKjpcWREFw5u4U_efy0UkRbq692QxC87k';
-$bot = new PhpGram($token);
-
-
-$botInfo = $bot->getMe();
-echo 'Bot Username: ' . $botInfo['result']['username'] . PHP_EOL;
-
+echo "Hello World!";
 

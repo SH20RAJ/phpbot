fd2c3937be110bdfc9b8c1cf4368da72736aec15
sh20raj <shaswatraj3@gmail.com>
2024-07-18 09:07:57 +0530

    Refactor index.php, improve DNS command handling and add HTML parsing option

--- hackersplay/index.php
+++ hackersplay/index.php
@@ -56,7 +56,7 @@ elseif(strpos($command, '/dns') === 0) {
     $url = 'https://bgp.he.net/dns/'.$url;
     $html = fetch_html_content($url);
     $innerHtml = extract_inner_html_by_id($html, 'dns' );
-    $innerHtml = "<b> DNS Lookup for: </b> <code>".$url."</code> \n\n".$innerHtml;
+    $innerHtml = "<b> DNS Lookup for: </b>";
     $bot->sendMessage($chatId, $innerHtml, ['parse_mode' => 'HTML']);
 }
 

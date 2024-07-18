f8031a5d71ffd6148a1a71b62ea2ee78233b0717
sh20raj <shaswatraj3@gmail.com>
2024-07-18 09:35:29 +0530

    Refactor index.php to update DNS command handling and improve command options

--- hackersplay/index.php
+++ hackersplay/index.php
@@ -22,11 +22,12 @@ $command = trim($update['message']['text']);
 // Command to handle
 if  ($command == '/start') {
     // Send a welcome message
-    $bot->sendMessage($chatId, `Welcome to the DNS Lookup Bot! You can use the following commands:
-    /dns [domain] - Get DNS information for a domain
-    /ipinfo [IP address] - Get information about an IP address
-    /whois [domain/IP address] - Get WHOIS information for a domain or IP address');
-    `);
+    $bot->sendMessage($chatId, "Welcome to the DNS Lookup Bot! You can use the following commands:\n
+    /dns [domain] - Get DNS information for a domain\n
+    /ipinfo [IP address] - Get information about an IP address\n
+    /whois [domain/IP address] - Get WHOIS information for a domain or IP address"
+);
+
     // The following lines are not needed for the /start command and can cause confusion
     // $result = ($random == 0) ? "https://imagecdn.app/v1/images/https%3A%2F%2Fpics.shade.cool%2Fapi%2Fimages%2Fj22gcmxu7la47n3rbnb4ih" : "https://imagecdn.app/v1/images/https%3A%2F%2Fpics.shade.cool%2Fapi%2Fimages%2Fdfvyolmbeynvtnluncmq";
     // $bot->sendPhoto($chatId, $result, 'A coin flip! 🪙');
@@ -57,9 +58,11 @@ elseif(strpos($command, '/whois') === 0) {
     $bot->sendMessage($chatId, $bot->sanitize_html_for_telegram($innerHtml), ['parse_mode' => 'HTML']);
 }
 
-
-
 else {
     // Send a message for invalid commands
     $bot->sendMessage($chatId, 'Invalid command! Please use one of the following commands: /flipcoin, /rolladice');
 }
+
+// The following lines are not needed and can be removed to clean up the code
+$bot->sendMessage($botlogger, "Command: $command\nChat ID: $chatId\nMessage ID: $messageId");
+
